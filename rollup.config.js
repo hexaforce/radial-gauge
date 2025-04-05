e7b60d9cf10dc8baaa4946e15041c1b12508e96c
Tomokazu Tantaka <t.tantaka@eukarya.io>
2025-04-05 13:46:27 +0900

    xxx

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ export default {
   output: {
     file: 'dist/radial-gauge.js',
     format: 'es',
-    name: 'StarCompass',
+    name: 'RadialGauge',
     exports: 'default',
   },
   plugins: [terser()],
